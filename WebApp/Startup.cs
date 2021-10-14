f0a7d85c9cd409b5b1ad57fcf61a7540cd93587c
WJ van Zyl <wjvanzyl0@gmail.com>
2021-10-14 13:08:24 +0200

    Reset

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -1,14 +1,8 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using WebApp.Data;
 
 namespace WebApp
@@ -40,7 +34,7 @@ namespace WebApp
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/FallbackPages/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -53,7 +47,7 @@ namespace WebApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
+                endpoints.MapFallbackToPage("/FallbackPages/_Host");
             });
         }
     }
