7a074f560f7a6209264bb9de04707246148458b6
WJ van Zyl <wjvanzyl0@gmail.com>
2021-11-18 07:26:38 +0200

    vghjm

---
+++ FileSystem/Models/File.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace FileSystem.Models
+{
+    class File
+    {
+        string path;
+        
+        public File(string path)
+        {
+            this.path = path;
+            System.Drawing.
+        }
+        
+
+
+    }
+}
