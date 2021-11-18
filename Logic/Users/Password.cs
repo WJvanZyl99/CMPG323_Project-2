5606dbd0ec28245e377880d1653bb4eae669a90f
WJ van Zyl <wjvanzyl0@gmail.com>
2021-11-18 13:20:42 +0200

    UserSessions

---
+++ Logic/Users/Password.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Users
+{
+    class Password
+    {
+        private string encoded_password;
+        public Password(string raw_password)
+        {
+            this.encoded_password = encode(raw_password);
+        }
+
+        public string get_encoded_password()
+        {
+            return this.encoded_password;
+        }
+
+        private string encode(string raw_data)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(raw_data));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
---
+++ Logic/Users/UserSession.cs
@@ -0,0 +1,36 @@
+﻿using Database.Models;
+using System;
+using System.Configuration;
+
+namespace Logic.Users
+{
+    class UserSession
+    {
+        private DateTime login_date_time;
+        private DateTime last_active_date_time;
+        private bool active = false;
+
+        public UserSession(User user, string password)
+        {
+            Password pass = new Password(password);
+            if(user.Password == pass.get_encoded_password())
+            {
+                this.login_date_time = this.last_active_date_time = DateTime.Now;
+                this.active = true;
+            }
+            else
+                this.active = false;
+        }
+
+        public bool is_active()
+        {
+            TimeSpan idle_max = new TimeSpan(0, Int32.Parse(ConfigurationManager.AppSettings["SessionUptimeLimts:IdleMax"]), 0);
+            TimeSpan session_max = new TimeSpan(0, Int32.Parse(ConfigurationManager.AppSettings["SessionUptimeLimts:SessionMax"]), 0);
+            if (active 
+                && (DateTime.Now - last_active_date_time) < idle_max
+                && (DateTime.Now - login_date_time) < session_max)
+                    return true;
+            return false;
+        }
+    }
+}
