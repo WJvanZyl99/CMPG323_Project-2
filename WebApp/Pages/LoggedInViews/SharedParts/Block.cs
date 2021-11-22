dc871dbd3ce58eb966e5a22447a70ffe91a55d02
WJ van Zyl <wjvanzyl0@gmail.com>
2021-11-22 22:19:01 +0200

    improvements

---
+++ WebApp/Pages/LoggedInViews/SharedParts/Block.cs
@@ -0,0 +1,21 @@
+﻿using Database.Models;
+
+namespace WebApp.Pages.LoggedInViews.SharedParts
+{
+    public class Block
+    {
+        public Image img;
+        public string path;
+        public bool hasDelete = false;
+        public bool showShared = false;
+        public bool hasShare = false;
+        public bool hasSharedWith = false;
+        public bool hasShareDelete = false;
+
+        public Block(Image image, string path)
+        {
+            this.img = image;
+            this.path = path;
+        }
+    }
+}
